767020385f853ca096552abd959af0da20443fe5
Felipe Vidal <felipevgevaerd@gmail.com>
2023-05-28 23:17:13 -0300

    Adicionando o processo de upload e novas alterações

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { AuthRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // TODAS URLS DE FRONT END PODERÃO ACESSAR O BACK-END
@@ -15,6 +25,7 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 
+app.register(uploadRoutes)
 app.register(AuthRoutes)
 app.register(memoriesRoutes)
 
